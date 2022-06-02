25e87171171cbccdf609847cb128b080559c77ec
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-06-02 21:18:06 +0200

    feat(app): log the value of a potentiometer

--- firmware/totem_app/src/main.rs
+++ firmware/totem_app/src/main.rs
@@ -1,4 +1,4 @@
-// totem_app - A totem for music festivals, built with love to spread love.
+// Totem - A totem for music festivals, built with love to spread love.
 // Copyright (C) 2022 Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
 //
 // This program is free software: you can redistribute it and/or modify
@@ -21,15 +21,47 @@
 use defmt_rtt as _;
 use panic_probe as _;
 
-#[rtic::app(device = stm32l4xx_hal::pac, dispatchers = [])]
+#[rtic::app(device = stm32l4xx_hal::pac, dispatchers = [TIM2])]
 mod app {
-    use stm32l4xx_hal::prelude::*;
+    use embedded_hal::blocking::delay::DelayUs;
+    use stm32l4xx_hal::{
+        adc::ADC,
+        gpio::{Analog, PA0},
+        prelude::*,
+    };
+    use systick_monotonic::Systick;
+
+    /// A basic Delay using `cortex_m::asm::delay`.
+    pub struct AsmDelay {
+        /// The AHB Frequency in Hz.
+        ahb_frequency: u32,
+    }
+
+    impl AsmDelay {
+        /// Initialises an AsmDelay.
+        pub fn new(ahb_frequency: u32) -> Self {
+            Self { ahb_frequency }
+        }
+    }
+
+    impl DelayUs<u32> for AsmDelay {
+        fn delay_us(&mut self, us: u32) {
+            let tick = (us as u64) * (self.ahb_frequency as u64) / 1_000_000;
+            cortex_m::asm::delay(tick as u32);
+        }
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type Monotonic = Systick<100>;
 
     #[shared]
     struct SharedResources {}
 
     #[local]
-    struct LocalResources {}
+    struct LocalResources {
+        adc: ADC,
+        p1: PA0<Analog>,
+    }
 
     #[init]
     fn init(
@@ -37,18 +69,38 @@ mod app {
     ) -> (SharedResources, LocalResources, init::Monotonics) {
         defmt::info!("Firmware starting...");
 
-        let _cp = cx.core;
+        let cp = cx.core;
         let dp = cx.device;
 
         // Clock configuration.
         let mut rcc = dp.RCC.constrain();
         let mut flash = dp.FLASH.constrain();
         let mut pwr = dp.PWR.constrain(&mut rcc.apb1r1);
-        let _clocks = rcc.cfgr.freeze(&mut flash.acr, &mut pwr);
+        let clocks = rcc.cfgr.sysclk(80.MHz()).freeze(&mut flash.acr, &mut pwr);
+
+        let monotonic = Systick::new(cp.SYST, 80_000_000);
+
+        let mut delay = AsmDelay::new(clocks.sysclk().to_Hz());
+        let adc = ADC::new(
+            dp.ADC1,
+            dp.ADC_COMMON,
+            &mut rcc.ahb2,
+            &mut rcc.ccipr,
+            &mut delay,
+        );
+
+        let mut gpioa = dp.GPIOA.split(&mut rcc.ahb2);
+        let p1 = gpioa.pa0.into_analog(&mut gpioa.moder, &mut gpioa.pupdr);
 
         defmt::info!("Firmware initialised!");
 
-        (SharedResources {}, LocalResources {}, init::Monotonics())
+        print_value::spawn().unwrap();
+
+        (
+            SharedResources {},
+            LocalResources { adc, p1 },
+            init::Monotonics(monotonic),
+        )
     }
 
     #[idle]
@@ -56,5 +108,13 @@ mod app {
         loop {
             continue;
         }
+    }
+
+    #[task(local = [adc, p1])]
+    fn print_value(cx: print_value::Context) {
+        print_value::spawn_at(monotonics::now() + 10.millis()).unwrap();
+
+        let value = cx.local.adc.read(cx.local.p1).unwrap();
+        defmt::info!("Value: {}", value);
     }
 }
