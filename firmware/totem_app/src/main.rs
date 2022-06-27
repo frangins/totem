cce82db235413189bec27262fd0e0fd9b9ce9675
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-06-27 21:14:35 +0200

    feat(app): illuminate the LEDs with a rainbow chaser

--- firmware/totem_app/src/main.rs
+++ firmware/totem_app/src/main.rs
@@ -23,11 +23,19 @@ use panic_probe as _;
 
 #[rtic::app(device = totem_board::pac, dispatchers = [TIM2])]
 mod app {
+    use embedded_time::{duration::Seconds, rate::Hertz};
+    use led_effects::{
+        chaser::{OneParameterChaser, RainbowChaser},
+        sequence::Rainbow,
+        time::TimeConfig,
+    };
+    use smart_leds::{brightness, colors::BLUE, SmartLedsWrite};
     use systick_monotonic::Systick;
+
     use totem_board::{
         board::Board,
-        constants::LED_BUFFER_SIZE,
-        peripheral::{R1, R2, R3, S1},
+        constants::{LED_BUFFER_SIZE, NUM_LEDS},
+        peripheral::{LedStrip, R1, R2, R3, S1},
         prelude::*,
     };
     use totem_ui::UI;
@@ -41,7 +49,12 @@ mod app {
     #[local]
     struct LocalResources {
         ui: UI<R1, R2, R3, S1>,
+        led_strip: LedStrip,
+        time_config: TimeConfig,
+        chaser: RainbowChaser<Rainbow<NUM_LEDS>, NUM_LEDS>,
     }
+
+    const REFRESH_RATE: Hertz = Hertz(50);
 
     #[init(local = [led_buffer: [u8; LED_BUFFER_SIZE] = [0; LED_BUFFER_SIZE]])]
     fn init(
@@ -65,10 +78,13 @@ mod app {
             b2: _,
             microphone: _,
             p_adc,
-            led_strip: _,
+            led_strip,
         } = Board::init(dp, cx.local.led_buffer);
 
         let ui = UI::new(p_adc, r1, r2, r3, s1);
+
+        let time_config = TimeConfig::new(REFRESH_RATE, Seconds(2));
+        let chaser = RainbowChaser::new(BLUE, &time_config);
 
         defmt::info!("Firmware initialised!");
 
@@ -76,7 +92,12 @@ mod app {
 
         (
             SharedResources {},
-            LocalResources { ui },
+            LocalResources {
+                ui,
+                led_strip,
+                time_config,
+                chaser,
+            },
             init::Monotonics(monotonic),
         )
     }
@@ -88,12 +109,25 @@ mod app {
         }
     }
 
-    #[task(local = [ui])]
+    #[task(local = [ui, led_strip, time_config, chaser])]
     fn update(cx: update::Context) {
-        update::spawn_at(monotonics::now() + 10.millis()).unwrap();
+        let update::LocalResources {
+            ui,
+            led_strip,
+            time_config,
+            chaser,
+        } = cx.local;
 
-        let ui = cx.local.ui;
         let ui_state = ui.read_state();
-        defmt::info!("UI State: {:?}", ui_state);
+        defmt::debug!("UI State: {:?}", ui_state);
+
+        let period = (1000 / time_config.refresh_rate.0).millis();
+        update::spawn_at(monotonics::now() + period).unwrap();
+
+        if let Some(sequence) = chaser.next() {
+            led_strip
+                .write(brightness(sequence, ui_state.brightness.value()))
+                .unwrap();
+        }
     }
 }
